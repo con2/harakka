794b8602d777f643d5fa811b9569823b1c87f17f
Maria Aluko <maria.alukoo@gmail.com>
2025-03-31 11:00:52 +0300

    SCRUM-67 Set up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <p>
+          Hello there!
+        </p>
+        <Button className="cursor-pointer">Click Me!</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
