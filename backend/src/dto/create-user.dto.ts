eff13f3b4d0a3afb5a71b6d2ca028252626d7d47
Jana Stefanie <garschtubald@gmail.com>
2025-04-03 10:46:33 +0300

    bugfix: create user works now

--- backend/src/dto/create-user.dto.ts
+++ backend/src/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
 export class CreateUserDto {
     email: string;
-    name?: string;
+    password: string;
+    full_name: string;
+    visible_name?: string;
+    phone?: string;
+    role?: string;
+    preferences?: Record<string, any>;
   }
   
