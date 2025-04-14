f5ba620af3a482c0e6f27545858e65e40d3d7840
Vladimir Beliakov <ermegilius@gmail.com>
2025-04-14 16:32:08 +0300

    SCRUM-14 feat(cart): user can add items to the cart from ItemsDetails

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import usersReducer from "./slices/usersSlice";
-import itemsReducer from "./slices/itemsSlice"
+import usersReducer from './slices/usersSlice';
+import itemsReducer from './slices/itemsSlice';
+import cartReducer from './slices/cartSlice';
 
 // add slices in the reducer object
 export const store = configureStore({
   reducer: {
     users: usersReducer,
-    items: itemsReducer
-  }
+    items: itemsReducer,
+    cart: cartReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
