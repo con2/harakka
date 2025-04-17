b68dc364d3dfc7229a311632d1326d8f625296cf
Jana Stefanie <garschtubald@gmail.com>
2025-04-17 14:14:51 +0300

    add update, delete, cancel and reject functionality

--- backend/src/controllers/booking.controller.ts
+++ backend/src/controllers/booking.controller.ts
@@ -1,16 +1,17 @@
-import { Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Post,
-    Put,
-    Req,
-    UseGuards, } from '@nestjs/common';
-import { BookingService } from '../services/booking.service';
-import { CreateBookingDto } from '../dto/create-booking.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+} from "@nestjs/common";
+import { BookingService } from "../services/booking.service";
+import { CreateBookingDto } from "../dto/create-booking.dto";
 
-@Controller('bookings')
+@Controller("bookings")
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
 
@@ -21,24 +22,46 @@ export class BookingController {
 
   @Post()
   async createBooking(@Body() dto: CreateBookingDto, @Req() req: any) {
-    const userEmail = req.user?.email ?? '';
+    const userEmail = req.user?.email ?? "";
     return this.bookingService.createBooking(dto, userEmail);
   }
 
-  @Put(':id/confirm')
-  async confirm(@Param('id') id: string) {
+  @Put(":id/confirm")
+  async confirm(@Param("id") id: string) {
     return this.bookingService.confirmBooking(id);
   }
 
-/*   @Delete(':id/reject')
-  async reject(@Param('id') id: string) {
-    return this.bookingService.rejectBooking(id);
-  } */
-
-  @Delete(':id/cancel')
-  async cancel(@Param('id') id: string, @Req() req: any) {
+  @Put(":id/update")
+  async updateBooking(
+    @Param("id") id: string,
+    @Body("items") items: any[],
+    @Req() req: any,
+  ) {
     const userId = req.user?.id;
-    return this.bookingService.cancelBooking(id, userId);
+    return this.bookingService.updateBooking(id, userId, items);
+  }
+
+  @Put(":id/reject")
+  async reject(@Param("id") id: string, @Req() req: any) {
+    const userId = req.user?.id;
+    return this.bookingService.rejectBooking(id, userId);
+  }
+
+  @Delete(":id/cancel")
+  async cancel(@Param("id") id: string, @Req() req: any) {
+    const userId = req.user?.id;
+    return this.bookingService.cancelOwnBooking(id, userId);
+  }
+
+  @Delete(":id/delete")
+  async delete(@Param("id") id: string, @Req() req: any) {
+    const userId = req.user?.id;
+    return this.bookingService.deleteBooking(id, userId);
+  }
+
+  @Post(":id/return")
+  async returnItems(@Param("id") id: string) {
+    return this.bookingService.returnItems(id);
   }
 }
 // handles the booking process, including creating, confirming, rejecting, and canceling bookings.
