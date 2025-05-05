a400816b7c9b6d605874d8709944aedc9d70b077
Vladimir Beliakov <ermegilius@gmail.com>
2025-05-05 16:40:35 +0300

    SCRUM-127 refactor(orders): add filtering to MyOrders and OrderList and consistant style action buttons

--- frontend/src/components/MyProfile.tsx
+++ frontend/src/components/MyProfile.tsx
@@ -2,20 +2,28 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { selectSelectedUser, updateUser } from "@/store/slices/usersSlice";
 import { Button } from "@/components/ui/button";
-import MyOrders from './MyOrders'; 
+import MyOrders from "./MyOrders";
 import { useEffect, useState } from "react";
 import { Avatar } from "./ui/avatar";
 import profilePlaceholder from "../assets/profilePlaceholder.png";
 import { toast } from "sonner";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 const MyProfile = () => {
   const dispatch = useAppDispatch();
   const selectedUser = useAppSelector(selectSelectedUser);
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  // Get tab from URL query parameter or default to "user-details"
+  const activeTab = searchParams.get("tab") || "user-details";
 
   const [name, setName] = useState(selectedUser?.full_name || "");
   const [email, setEmail] = useState(selectedUser?.email || "");
   const [phone, setPhone] = useState(selectedUser?.phone || "");
-  const [visibleName, setVisibleName] = useState(selectedUser?.visible_name || "");
+  const [visibleName, setVisibleName] = useState(
+    selectedUser?.visible_name || "",
+  );
   // const [preferences, setPreferences] = useState(selectedUser?.preferences || "");
   const profileImage = profilePlaceholder;
 
@@ -29,43 +37,43 @@ const MyProfile = () => {
     }
   }, [selectedUser]);
 
+  // Handle tab change with URL update
+  const handleTabChange = (value: string) => {
+    navigate(`/profile?tab=${value}`);
+  };
+
   const handleSaveChanges = () => {
     if (selectedUser) {
-      const updatedUserData = { 
-        full_name: name, 
-        email, 
-        phone, 
-        visible_name: visibleName, 
-        // preferences: typeof preferences === "string" ? undefined : preferences 
+      const updatedUserData = {
+        full_name: name,
+        email,
+        phone,
+        visible_name: visibleName,
+        // preferences: typeof preferences === "string" ? undefined : preferences
       };
-      
+
       // Dispatch the updateUser action
       dispatch(updateUser({ id: selectedUser.id, data: updatedUserData }))
         .unwrap()
         .then(() => {
-          toast.success('Profile updated successfully!'); 
+          toast.success("Profile updated successfully!");
         })
         .catch(() => {
-          toast.error('Failed to update profile.');
+          toast.error("Failed to update profile.");
         });
     }
   };
 
   return (
     <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 md:px-8 m-10 gap-20 box-shadow-lg rounded-lg bg-white">
-
-      <Tabs defaultValue="user-details" className="w-full bg-slate-50 p-4 rounded-lg mb-10">
+      <Tabs
+        value={activeTab}
+        onValueChange={handleTabChange}
+        className="w-full bg-slate-50 p-4 rounded-lg mb-10"
+      >
         <TabsList className="grid w-full grid-cols-2 mb-8 gap-4">
-          <TabsTrigger
-            value="user-details"
-          >
-            My Profile
-          </TabsTrigger>
-          <TabsTrigger
-            value="orders"
-          >
-            My Orders
-          </TabsTrigger>
+          <TabsTrigger value="user-details">My Profile</TabsTrigger>
+          <TabsTrigger value="orders">My Orders</TabsTrigger>
         </TabsList>
 
         {/* User Details Tab */}
@@ -75,45 +83,64 @@ const MyProfile = () => {
               <div className="flex flex-row items-start">
                 <div className="flex flex-1 items-center justify-left p-2 space-y-4">
                   <Avatar className="w-24 h-24 rounded-full border-1 border-secondary flex">
-                    <img src={profileImage || ""} alt="Profile" className="w-full h-full object-cover rounded-full" />
+                    <img
+                      src={profileImage || ""}
+                      alt="Profile"
+                      className="w-full h-full object-cover rounded-full"
+                    />
                   </Avatar>
                 </div>
-                
+
                 {/* Editable Fields for Name, Email, and Phone */}
                 <div className="flex flex-2 flex-col p-2 space-y-4">
                   <div>
-                    <label htmlFor="name" className="block text-xs font-medium text-gray-700">Full Name</label>
+                    <label
+                      htmlFor="name"
+                      className="block text-xs font-medium text-gray-700"
+                    >
+                      Full Name
+                    </label>
                     <input
                       id="name"
                       type="text"
                       value={name}
                       onChange={(e) => setName(e.target.value)}
                       className="mt-1 p-3 w-full border border-gray-300 rounded-md text-sm text-gray-600 focus:ring-2 focus:ring-secondary focus:outline-none"
-                      style={{ fontSize: '0.875rem', height: '40px' }}
+                      style={{ fontSize: "0.875rem", height: "40px" }}
                     />
                   </div>
 
                   <div>
-                    <label htmlFor="email" className="block text-xs font-medium text-gray-700">Email</label>
+                    <label
+                      htmlFor="email"
+                      className="block text-xs font-medium text-gray-700"
+                    >
+                      Email
+                    </label>
                     <input
                       id="email"
                       type="email"
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       className="mt-1 p-3 w-full border border-gray-300 rounded-md text-sm text-gray-600 focus:ring-2 focus:ring-secondary focus:outline-none"
-                      style={{ fontSize: '0.875rem', height: '40px' }}
+                      style={{ fontSize: "0.875rem", height: "40px" }}
                     />
                   </div>
 
                   <div>
-                    <label htmlFor="phone" className="block text-xs font-medium text-gray-700">Phone</label>
+                    <label
+                      htmlFor="phone"
+                      className="block text-xs font-medium text-gray-700"
+                    >
+                      Phone
+                    </label>
                     <input
                       id="phone"
                       type="text"
                       value={phone}
                       onChange={(e) => setPhone(e.target.value)}
                       className="mt-1 p-3 w-full border border-gray-300 rounded-md text-sm text-gray-600 focus:ring-2 focus:ring-secondary focus:outline-none"
-                      style={{ fontSize: '0.875rem', height: '40px' }}
+                      style={{ fontSize: "0.875rem", height: "40px" }}
                     />
                   </div>
                 </div>
@@ -121,14 +148,19 @@ const MyProfile = () => {
                 {/* Placeholder div for spacing */}
                 <div className="flex flex-2 flex-col p-2 space-y-4">
                   <div>
-                    <label htmlFor="visibleName" className="block text-xs font-medium text-gray-700">Visible Name</label>
+                    <label
+                      htmlFor="visibleName"
+                      className="block text-xs font-medium text-gray-700"
+                    >
+                      Visible Name
+                    </label>
                     <input
                       id="visibleName"
                       type="text"
                       value={visibleName}
                       onChange={(e) => setVisibleName(e.target.value)}
                       className="mt-1 p-3 w-full border border-gray-300 rounded-md text-sm text-gray-600 focus:ring-2 focus:ring-secondary focus:outline-none"
-                      style={{ fontSize: '0.875rem', height: '40px' }}
+                      style={{ fontSize: "0.875rem", height: "40px" }}
                     />
                   </div>
                   {/* <div>
@@ -163,7 +195,7 @@ const MyProfile = () => {
 
         {/* Orders Tab */}
         <TabsContent value="orders">
-          <MyOrders />  {/* Embedding your existing MyOrders component */}
+          <MyOrders /> {/* Embedding your existing MyOrders component */}
         </TabsContent>
       </Tabs>
     </div>
