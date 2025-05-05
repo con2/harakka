053f9f9e2a63ac4c4305df79977c21a2eacbc940
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-05-05 22:39:32 +0300

    add endpoint for getAvailableItems and seperate method in booking class

--- backend/src/utils/booking.utils.ts
+++ backend/src/utils/booking.utils.ts
@@ -13,21 +13,19 @@ export async function calculateAvailableQuantity(
     .eq("item_id", itemId)
     .in("status", ["pending", "confirmed"])
     .or(`and(start_date.lte.${endDate},end_date.gte.${startDate})`);
-  console.log(overlapping); // buchungen
+
   if (error) {
     throw new Error("Error checking the bookings");
   }
 
   const booked =
     overlapping?.reduce((sum, o) => sum + (o.quantity || 0), 0) ?? 0;
-  console.log(booked); // schaut wieviele items in den buchungen drin sind
 
   const { data: item, error: itemError } = await supabase
     .from("storage_items")
     .select("items_number_total")
     .eq("id", itemId)
     .single();
-  console.log(item?.items_number_total); // wieviele es gibt- alle items
 
   if (itemError || !item) {
     throw new Error("Error when retrieving/ calling item.total");
