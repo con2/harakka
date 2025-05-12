40747803f7aed5c0ec057a7e90191145b7945338
Maria Aluko <maria.alukoo@gmail.com>
2025-05-12 14:17:27 +0300

    bugfix: replace all toasts with a global toast

--- frontend/src/components/Admin/OrderRejectButton.tsx
+++ frontend/src/components/Admin/OrderRejectButton.tsx
@@ -3,6 +3,7 @@ import { toast } from "sonner";
 import { useAppDispatch } from "@/store/hooks";
 import { XCircle } from "lucide-react";
 import { rejectOrder } from "@/store/slices/ordersSlice";
+import { toastConfirm } from "../ui/toastConfirm";
 
 const OrderRejectButton = ({
   id,
@@ -19,33 +20,20 @@ const OrderRejectButton = ({
       return;
     }
 
-    toast.custom((t) => (
-      <div className="bg-white dark:bg-primary text-primary dark:text-white border border-zinc-200 dark:border-primary rounded-xl p-4 w-[360px] shadow-lg flex flex-col gap-3">
-        <div className="font-semibold text-lg">Confirm Rejection</div>
-        <div className="text-sm">
-          Are you sure you want to reject this order?
-        </div>
-        <div className="flex justify-between gap-2">
-          <Button className="addBtn" size="md" onClick={() => toast.dismiss(t)}>
-            Cancel
-          </Button>
-          <Button
-            className="bg-red-500 text-white hover:bg-red-700 border-1 border-red-500 rounded-2xl"
-            onClick={async () => {
-              toast.dismiss(t); // dismiss confirmation toast
-              await toast.promise(dispatch(rejectOrder(id)).unwrap(), {
-                loading: "Rejecting order...",
-                success: "Order has been successfully rejected.",
-                error: "Failed to reject the order.",
-              });
-              closeModal();
-            }}
-          >
-            Confirm
-          </Button>
-        </div>
-      </div>
-    ));
+    toastConfirm({
+      title: "Confirm Rejection",
+      description: "Are you sure you want to reject this order?",
+      confirmText: "Confirm",
+      cancelText: "Cancel",
+      onConfirm: async () => {
+        await toast.promise(dispatch(rejectOrder(id)).unwrap(), {
+          loading: "Rejecting order...",
+          success: "Order has been successfully rejected.",
+          error: "Failed to reject the order.",
+        });
+        closeModal();
+      },
+    });
   };
 
   return (
