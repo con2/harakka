eec0487023df597b7c16563b4c0af1e44bddf6a7
Vladimir Beliakov <ermegilius@gmail.com>
2025-05-13 20:09:02 +0300

    SCRUM-134: add translation context and custom hook. Apply to nav and item details

---
+++ frontend/src/translations/index.ts
@@ -0,0 +1,115 @@
+export const t = {
+  common: {},
+  itemDetails: {
+    buttons: {
+      save: {
+        fi: "Tallenna",
+        en: "Save",
+      },
+      cancel: {
+        fi: "Peruuta",
+        en: "Cancel",
+      },
+      delete: {
+        fi: "Poista",
+        en: "Delete",
+      },
+      back: {
+        fi: "Takaisin",
+        en: "Back",
+      },
+    },
+    items: {
+      addToCart: {
+        fi: "Lisää koriin",
+        en: "Add to Cart",
+      },
+      quantity: {
+        fi: "Määrä",
+        en: "Quantity",
+      },
+    },
+    locations: {
+      locationInfo: {
+        fi: "Sijaintitiedot",
+        en: "Location Information",
+      },
+      location: {
+        fi: "Sijainti",
+        en: "Location",
+      },
+      address: {
+        fi: "Osoite",
+        en: "Address",
+      },
+    },
+    info: {
+      noDates: {
+        fi: "Varataksesi tämän tuotteen, valitse ensin varauspäivät ",
+        en: "To book this item, please first select booking dates ",
+      },
+      here: {
+        fi: "tästä",
+        en: "here",
+      },
+      timeframe: {
+        fi: "Valittu varaus",
+        en: "Selected booking",
+      },
+    },
+  },
+  navigation: {
+    home: {
+      fi: "Etusivu",
+      en: "Home",
+    },
+    myProfile: {
+      fi: "Profiilini",
+      en: "My Profile",
+    },
+    admin: {
+      fi: "Ylläpitäjä",
+      en: "Admin",
+    },
+    storage: {
+      fi: "Varasto",
+      en: "Storage",
+    },
+    guides: {
+      fi: "Oppaat",
+      en: "Guides",
+    },
+    toast: {
+      title: {
+        fi: "Vahvista uloskirjautuminen",
+        en: "Confirm Logout",
+      },
+      description: {
+        fi: "Haluatko varmasti kirjautua ulos? Tämä lopettaa nykyisen istuntosi.",
+        en: "Are you sure you want to log out? This will end your current session.",
+      },
+      confirmText: {
+        fi: "Kirjaudu ulos",
+        en: "Log Out",
+      },
+      cancelText: {
+        fi: "Peruuta",
+        en: "Cancel",
+      },
+      success: {
+        fi: "Uloskirjautuminen peruttu.",
+        en: "Logout canceled.",
+      },
+    },
+  },
+  login: {
+    login: {
+      fi: "Kirjaudu sisään",
+      en: "Login",
+    },
+    logout: {
+      fi: "Kirjaudu ulos",
+      en: "Logout",
+    },
+  },
+};
