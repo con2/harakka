1eb45bca64570bab397903df48a802c7bcafabf3
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-05-13 21:31:04 +0300

    add try to add react mail to backend

--- backend/src/controllers/app.controller.ts
+++ backend/src/controllers/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from '../services/app.service';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { AppService } from "../services/app.service";
+import { MailService } from "../mail/mail.service";
+import Email from "../emails";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly mailService: MailService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("send-email")
+  async sendMail(
+    @Body() sendMailDto: { email: string; subject: string },
+  ): Promise<string> {
+    await this.mailService.sendMail({
+      ...sendMailDto,
+      template: Email({ url: "http://example.com" }),
+    });
+
+    return "Email sent successfully";
   }
 }
