731a27c90a6110a7c8dae1ab281bc8ee466302b5
Maria Aluko <maria.alukoo@gmail.com>
2025-05-17 13:56:39 +0300

    UI: update UI as recommended

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -11,16 +11,18 @@ const LandingPage = () => {
     <div className="relative min-h-screen w-full overflow-hidden">
       {/* Background Image */}
       <div
-        className="absolute inset-0 bg-cover bg-center -z-10"
-        style={{ backgroundImage: `url(${illusiaImage})` }}
+        className="absolute inset-[-8px] bg-cover bg-center -z-10 blur-[5px]"
+        style={{ 
+          backgroundImage: `url(${illusiaImage})`,
+        }}
       />
 
       {/* Content on top of image */}
-      <div className="flex flex-col justify-center items-center min-h-screen bg-black/50 text-white text-center px-4 gap-2">
-        <h2 className="text-5xl font-bold mb-4 text-white">
+      <div className="flex flex-col justify-center items-center min-h-screen text-white text-center px-4 gap-2">
+        <h2 className="text-5xl font-bold mb-4 text-primary">
           {t.landingPage.heading[lang]}
         </h2>
-        <p className="mb-6 text-xl">{t.landingPage.subheading[lang]}</p>
+        <p className="mb-6 text-xl text-primary">{t.landingPage.subheading[lang]}</p>
         <Button
           onClick={() => (window.location.href = "/storage")}
           className="bg-secondary text-white border:secondary font-semibold px-6 py-5 rounded-lg shadow hover:bg-white hover:text-secondary hover:border-secondary transition"
