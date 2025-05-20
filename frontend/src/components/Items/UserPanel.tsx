454ee3cf90037f70eb759457b90325f5d01136e7
Maria Aluko <maria.alukoo@gmail.com>
2025-05-20 18:07:34 +0300

    bugfix: fix filters in Item Storage page

--- frontend/src/components/Items/UserPanel.tsx
+++ frontend/src/components/Items/UserPanel.tsx
@@ -39,14 +39,14 @@ const UserPanel = () => {
           return itemType;
         })
         .filter(Boolean)
-        .map((type) => type.charAt(0).toUpperCase() + type.slice(1))
+        .map((type) => type)
         .sort((a, b) => a.localeCompare(b)),
     ),
   );
   const [showAllItemTypes, setShowAllItemTypes] = useState(false);
   const visibleItemTypes = showAllItemTypes
     ? uniqueItemTypes
-    : uniqueItemTypes.slice(0, 7);
+    : uniqueItemTypes.slice(0, 5);
 
   // filter states
   const [filters, setFilters] = useState<{
@@ -65,7 +65,7 @@ const UserPanel = () => {
     locationIds: [],
   });
 
-  // Handle filter change (you can modify this based on your filter UI)
+  // Handle filter change
   const handleFilterChange = (filterKey: string, value: FilterValue) => {
     setFilters((prevFilters) => ({
       ...prevFilters,
@@ -74,18 +74,19 @@ const UserPanel = () => {
   };
 
   const countActiveFilters = () => {
-    let count = 0;
-    if (
-      filters.itemsNumberAvailable[0] !== 0 ||
-      filters.itemsNumberAvailable[1] !== 100
-    )
-      count++;
-    if (filters.averageRating.length > 0) count++;
-    if (filters.itemTypes.length > 0) count++;
-    if (filters.tagIds.length > 0) count++;
-    if (filters.locationIds.length > 0) count++;
-    return count;
-  };
+  let count = 0;
+  if (
+    filters.itemsNumberAvailable[0] !== 0 ||
+    filters.itemsNumberAvailable[1] !== 100
+  ) {
+    count++;
+  }
+  count += filters.averageRating.length;
+  count += filters.itemTypes.length;
+  count += filters.tagIds.length;
+  count += filters.locationIds.length;
+  return count;
+};
 
   // Mobile filter toggle visibility state
   const [isFilterVisible, setIsFilterVisible] = useState(false);
@@ -106,44 +107,48 @@ const UserPanel = () => {
             <div className="flex items-center justify-between my-2">
               <h3 className="text-secondary font-bold mb-0">Filters</h3>
               <div className="flex items-center gap-2">
+                 {/* Clear filters button */}
+                {countActiveFilters() > 0 && (
+                  <div className="flex justify-start">
+                    <Button
+                      variant="ghost"
+                      size={"sm"}
+                      className="text-xs px-1 bg-white text-highlight2 border-highlight2 hover:bg-highlight2 hover:text-white"
+                      onClick={() =>
+                        setFilters({
+                          isActive: true,
+                          averageRating: [],
+                          itemsNumberAvailable: [0, 100],
+                          itemTypes: [],
+                          tagIds: [],
+                          locationIds: [],
+                        })
+                      }
+                    >
+                      Clear Filters
+                    </Button>
+                  </div>
+                )}
                 <div className="text-xs text-muted-foreground">
                   {countActiveFilters()} {t.userPanel.filters.active[lang]}
                 </div>
                 <SlidersIcon className="w-5 h-5 text-slate-500" />
               </div>
             </div>
-
-            {/* Clear filters button */}
-            {countActiveFilters() > 0 && (
-              <div className="flex justify-start">
-                <Button
-                  size={"sm"}
-                  className="text-xs px-1 bg-white text-highlight2 border-highlight2 hover:bg-highlight2 hover:text-white"
-                  onClick={() =>
-                    setFilters({
-                      isActive: true,
-                      averageRating: [],
-                      itemsNumberAvailable: [0, 100],
-                      itemTypes: [],
-                      tagIds: [],
-                      locationIds: [],
-                    })
-                  }
-                >
-                  Clear Filters
-                </Button>
-              </div>
-            )}
             <Separator className="my-4" />
 
             {/* Categories / item_types*/}
-            <div className="flex flex-col flex-wrap gap-3 text-sm my-6">
+            <div className="flex flex-col flex-wrap gap-3">
+              <label className="text-primary text-md block mb-0">
+                {" "}
+                {t.userPanel.filters.itemTypes[lang]}
+              </label>
               {visibleItemTypes.map((typeName) => {
                 const isSelected = filters.itemTypes?.includes(typeName);
                 return (
                   <span
                     key={typeName}
-                    className={`cursor-pointer justify-between flex items-center ${
+                    className={`cursor-pointer text-sm justify-between flex items-center ${
                       isSelected
                         ? "text-secondary font-bold"
                         : "text-slate-500 hover:text-secondary"
@@ -155,14 +160,14 @@ const UserPanel = () => {
                       handleFilterChange("itemTypes", updated);
                     }}
                   >
-                    {typeName} <ChevronRight className="w-4 h-4 inline" />
+                    {typeName.charAt(0).toUpperCase() + typeName.slice(1)} <ChevronRight className="w-4 h-4 inline" />
                   </span>
                 );
               })}
               {uniqueItemTypes.length > 5 && (
                 <Button
                   variant="ghost"
-                  className="text-left text-sm text-secondary mt-2"
+                  className="text-left text-sm text-secondary"
                   onClick={() => setShowAllItemTypes((prev) => !prev)}
                 >
                   {showAllItemTypes
@@ -199,7 +204,7 @@ const UserPanel = () => {
 
             {/* Locations filter section */}
             <div className="my-4">
-              <label className="text-primary font-medium block mb-4">
+              <label className="text-primary font-medium block mb-2">
                 {t.userPanel.locations.title[lang]}
               </label>
               <div className="flex flex-col gap-2">
@@ -208,9 +213,9 @@ const UserPanel = () => {
                   return (
                     <label
                       key={location.id}
-                      className={`flex items-center gap-2 cursor-pointer ${
+                      className={`flex items-center gap-2 text-sm cursor-pointer ${
                         isSelected
-                          ? "text-secondary font-medium"
+                          ? "text-secondary"
                           : "text-slate-600 hover:text-secondary"
                       }`}
                     >
@@ -228,7 +233,6 @@ const UserPanel = () => {
                         className="accent-secondary"
                       />
                       <div className="flex items-center gap-1">
-                        <MapPin className="h-3.5 w-3.5" />
                         <span>{location.name}</span>
                       </div>
                     </label>
@@ -240,7 +244,7 @@ const UserPanel = () => {
             <Separator className="my-4" />
             {/* Tags */}
             <div className="my-4">
-              <label className="text-primary block mb-6">
+              <label className="text-primary block mb-4">
                 {" "}
                 {t.userPanel.tags.title[lang]}
               </label>
@@ -313,53 +317,14 @@ const UserPanel = () => {
                 })}
               </div>
             </div>
-            <Separator className="my-4" />
-
-            {/* color filter */}
-            <div className="my-4">
-              <label className="text-primary font-medium block mb-6">
-                {t.userPanel.colors.title[lang]}
-              </label>
-              <div className="mt-2 mb-6 text-secondary text-center">
-                <div className="flex flex-row flex-wrap gap-3">
-                  <Button
-                    className="bg-red-500 w-8 h-8 rounded-full border-1 border-primary"
-                    onClick={() => handleFilterChange("color", "red")}
-                  ></Button>
-                  <Button
-                    className="bg-green-500 w-8 h-8 rounded-full border-1 border-primary"
-                    onClick={() => handleFilterChange("color", "green")}
-                  ></Button>
-                  <Button
-                    className="bg-blue-500 w-8 h-8 rounded-full border-1 border-primary"
-                    onClick={() => handleFilterChange("color", "blue")}
-                  ></Button>
-                  <Button
-                    className="bg-yellow-500 w-8 h-8 rounded-full border-1 border-primary"
-                    onClick={() => handleFilterChange("color", "yellow")}
-                  ></Button>
-                  <Button
-                    className="bg-purple-500 w-8 h-8 rounded-full border-1 border-primary"
-                    onClick={() => handleFilterChange("color", "purple")}
-                  ></Button>
-                  <Button
-                    className="bg-black w-8 h-8 rounded-full border-1 border-primary"
-                    onClick={() => handleFilterChange("color", "black")}
-                  ></Button>
-                  <Button
-                    className="bg-white-500 w-8 h-8 rounded-full border-1 border-primary"
-                    onClick={() => handleFilterChange("color", "white")}
-                  ></Button>
-                </div>
-              </div>
-            </div>
 
             {countActiveFilters() > 0 && (
               <div>
                 <Separator className="my-4" />
-                <div className="flex justify-center mt-4">
+                <div className="flex justify-center mt-2">
                   <Button
-                    className="text-xs px-1 py-0.5 bg-white text-highlight2 border-1 border-highlight2 hover:bg-highlight2 hover:text-white"
+                    variant={"outline"}
+                    size={"sm"}
                     onClick={() =>
                       setFilters({
                         isActive: true,
