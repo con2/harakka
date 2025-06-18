9662fbca299c774879abda43255e97214f604eba
Jonathan Gian <jongian90@gmail.com>
2025-06-18 17:59:08 +0300

    Applied middleware to UserController and got rid of all type errors

--- backend/src/modules/user/interfaces/user.interface.ts
+++ backend/src/modules/user/interfaces/user.interface.ts
@@ -1,7 +1,10 @@
-export interface User {
-  id: string;
-  email: string;
-  name?: string;
-  created_at: string;
-  updated_at?: string;
-}
+import { Database } from "src/types/supabase.types";
+
+/**
+ * @description Here are the proper Supabase types we should be using accross the backend for 
+ * clear types that change when we change our tables.
+ * Click the "Row" type to see the database schema.
+ * This type represents a single row in the user_profiles table.
+ */
+export type UserProfile = Database["public"]["Tables"]["user_profiles"]["Row"];
+export type UserAddress = Database["public"]["Tables"]["user_addresses"]["Row"];
