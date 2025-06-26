931e5c01377c7da385ba0af4c99b98eda69e55d0
Maria Aluko <maria.alukoo@gmail.com>
2025-06-26 13:31:16 +0300

    apply route config, login translation bugfix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import { LanguageProvider } from "./context/LanguageContext.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LanguageProvider>
       <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </LanguageProvider>
-  </StrictMode>,
+  </StrictMode>
 );
