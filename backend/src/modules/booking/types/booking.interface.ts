6a23e8a63382d50f27e058b5c06ab704020a32a2
Athina <athina.kantis@gmail.com>
2025-06-29 13:52:04 +0300

    update: move booking types content into booking.interface

--- backend/src/modules/booking/types/booking.interface.ts
+++ backend/src/modules/booking/types/booking.interface.ts
@@ -9,3 +9,18 @@ export interface BookingRequest {
   user_email?: string; // optional because for ddmin-booking
   items: BookingItem[];
 }
+export interface EnrichedItem {
+  item_id: string;
+  quantity: number;
+  start_date?: string;
+  end_date?: string;
+  translations?: {
+    fi: { item_name: string };
+    en: { item_name: string };
+  };
+  location_id?: string;
+}
+
+export type UserBookingOrder = {
+  order_items?: { storage_items?: { location_id?: string } }[];
+};
