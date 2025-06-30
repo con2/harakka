2d68e38b5873d190c7a9e4458fce11e3f38347f7
Athina <athina.kantis@gmail.com>
2025-06-30 13:52:35 +0300

    refactor: rename order-items to booking-items; add delete route

---
+++ backend/src/modules/booking_items/dto/new-booking-item.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+} from "class-validator";
+
+export class NewBookingItemDto {
+  @IsNotEmpty()
+  @IsString()
+  order_id: string;
+
+  @IsUUID()
+  item_id: string;
+
+  @IsUUID()
+  location_id: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  start_date: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  end_date: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  total_days: number;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+}
