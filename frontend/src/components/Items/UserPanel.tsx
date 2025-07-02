fcce85ca97d2aa62b16ca3cab01542ad0f3fffbd
Athina <athina.kantis@gmail.com>
2025-07-02 14:29:08 +0300

    remove duplicate code

--- frontend/src/components/Items/UserPanel.tsx
+++ frontend/src/components/Items/UserPanel.tsx
@@ -25,8 +25,7 @@ const UserPanel = () => {
   const filterRef = useRef<HTMLDivElement>(null); // Ref for the filter panel position
 
   useEffect(() => {
-    if (tags.length < 1)
-      dispatch(fetchAllTags({ page: 1, limit: 10 }));
+    if (tags.length < 1) dispatch(fetchAllTags({ page: 1, limit: 10 }));
     if (locations.length < 1)
       dispatch(fetchAllLocations({ page: 1, limit: 10 }));
   }, [dispatch, tags, locations]);
@@ -90,19 +89,6 @@ const UserPanel = () => {
     count += filters.locationIds.length;
     return count;
   };
-    let count = 0;
-    if (
-      filters.itemsNumberAvailable[0] !== 0 ||
-      filters.itemsNumberAvailable[1] !== 100
-    ) {
-      count++;
-    }
-    count += filters.averageRating.length;
-    count += filters.itemTypes.length;
-    count += filters.tagIds.length;
-    count += filters.locationIds.length;
-    return count;
-  };
 
   // Mobile filter toggle visibility state
   const [isFilterVisible, setIsFilterVisible] = useState(false);
@@ -118,9 +104,8 @@ const UserPanel = () => {
       {/* Sidebar: Filters Panel */}
       <aside
         ref={filterRef}
-        className={`${
-          isFilterVisible ? "block" : "hidden"
-        } md:flex md:flex-col md:min-h-[calc(100vh-60px)] w-full md:w-76 p-4 bg-white md:pb-10 fixed inset-0 z-40 md:static transition-all duration-300 ease-in-out md:overflow-visible overflow-y-auto`}
+        className={`${isFilterVisible ? "block" : "hidden"
+          } md:flex md:flex-col md:min-h-[calc(100vh-60px)] w-full md:w-76 p-4 bg-white md:pb-10 fixed inset-0 z-40 md:static transition-all duration-300 ease-in-out md:overflow-visible overflow-y-auto`}
         style={{
           top: "60px",
           backgroundColor: "#fff",
@@ -132,7 +117,6 @@ const UserPanel = () => {
             <div className="flex items-center justify-between my-2">
               <h3 className="text-secondary font-bold mb-0">Filters</h3>
               <div className="flex items-center gap-2">
-                {/* Clear filters button */}
                 {/* Clear filters button */}
                 {countActiveFilters() > 0 && (
                   <div className="flex justify-start">
