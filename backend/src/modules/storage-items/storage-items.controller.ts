cca87d9709ba01e8c20399991990b538e62a4f81
Athina <athina.kantis@gmail.com>
2025-07-07 09:17:59 +0300

    feat: add queryconstructor

--- backend/src/modules/storage-items/storage-items.controller.ts
+++ backend/src/modules/storage-items/storage-items.controller.ts
@@ -13,7 +13,8 @@ import {
 import { Request } from "express";
 import { StorageItemsService } from "./storage-items.service";
 import { SupabaseService } from "../supabase/supabase.service";
-import { Tables, TablesInsert, TablesUpdate } from "../../types/supabase.types"; // Import the Database type for type safety
+import { Tables } from "../../types/supabase.types"; // Import the Database type for type safety
+import { ValidItemOrder } from "./interfaces/storage-item.interface";
 // calls the methods of storage-items.service.ts & handles API req and forwards it to the server
 
 @Controller("storage-items") // api path: /storage-items = Base URL     // = HTTP-Controller
@@ -34,18 +35,54 @@ export class StorageItemsController {
     return this.storageItemsService.getAllItems(pageNum, limitNum);
   }
 
+  /**
+   * Get ordered and/or filtered items
+   * @param page What page number is requested
+   * @param limit How many rows to retrieve
+   * @param ascending If to sort order smallest-largest (e.g a-z) or descending (z-a). Default true / ascending.
+   * @param order_by What column to order the columns by. Default "created_at". See {Valid}
+   * @param searchquery Optional. Filter items by a string
+   * @returns Matching items
+   */
   @Get("ordered")
-  getOrderedBookings(
+  getOrderedItems(
     @Query("search") searchquery: string,
-    @Query("order") ordered_by: string,
+    @Query("order") ordered_by: ValidItemOrder,
     @Query("page") page: string = "1",
     @Query("limit") limit: string = "10",
     @Query("ascending") ascending: string = "true",
+    @Query("tags") tags: string,
+    @Query("active") active_filter: "active" | "inactive",
   ) {
     const pageNum = parseInt(page, 10);
     const limitNum = parseInt(limit, 10);
     const is_ascending = ascending.toLowerCase() === "true";
     return this.storageItemsService.getOrderedStorageItems(
+      pageNum,
+      limitNum,
+      is_ascending,
+      ordered_by,
+      searchquery,
+      tags,
+      active_filter,
+    );
+  }
+  @Get("test")
+  async getOrderedTableRows(
+    @Query("select") select: string = "*",
+    @Query("page") page: string = "1",
+    @Query("limit") limit: string = "10",
+    @Query("ascending") ascending: string = "true",
+    @Query("search") searchquery: string,
+    @Query("order") ordered_by?: string,
+  ) {
+    const pageNum = parseInt(page, 10);
+    const limitNum = parseInt(limit, 10);
+    const is_ascending = ascending.toLowerCase() === "true";
+
+    return await this.storageItemsService.supabaseTest(
+      "storage_items",
+      select,
       pageNum,
       limitNum,
       is_ascending,
