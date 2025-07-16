7c632d7451b656f86de86e2f71822615d031386a
Jonathan Gian <jongian90@gmail.com>
2025-07-16 15:13:04 +0300

    Removed unused tags types from common folder

--- common/tags.ts
+++
@@ -1,5 +0,0 @@
- import { Database } from "./database.types";
-
- export type Tag = Database["public"]["Tables"]["tags"]["Row"];
-export type TagUpdate = Database["public"]["Tables"]["tags"]["Update"];
-export type CreateTag = Database["public"]["Tables"]["tags"]["Insert"];
