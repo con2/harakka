7a2315735b9f249dfecb280c100c3642e0d01266
stabjana <garschtubald@gmail.com>
2025-07-18 11:05:09 +0300

    add org loc module, add org loc interface, add create org to service

--- backend/src/modules/organization-locations/organization_locations.service.ts
+++ backend/src/modules/organization-locations/organization_locations.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { SupabaseService } from "../supabase/supabase.service";
 import { getPaginationMeta } from "@src/utils/pagination";
+import { ApiSingleResponse } from "@common/response.types";
+import { Or } from "type-fest";
 
 @Injectable()
 export class OrganizationLocationsService {
@@ -29,6 +31,44 @@ export class OrganizationLocationsService {
       cont: count || 0,
       metadata: getPaginationMeta(count ?? 0, page, limit),
       statusText: "OK",
+    };
+  }
+
+  async getOrgLocById(id: string): Promise<OrgLocationRow | null> {
+    const client = this.supabaseServiceService.getServiceClient();
+    const { data, error } = await client
+      .from("organization_locations")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) handleSupabaseError(error);
+    return data ?? null;
+  }
+
+  async createOrgLoc(
+    req: AuthRequest,
+    dto: OrgLocationInsert,
+  ): Promise<ApiSingleResponse<OrgLocationRow>> {
+    this.assertCreatePermission(req, dto.organization_id);
+
+    dto.created_by = req.user.id;
+
+    const { data, error }: PostgresSingleResponse<OrgLocationRow> =
+      await req.supabase
+        .from("organization_locations")
+        .insert(dto)
+        .select("*")
+        .single();
+
+    if (error) handleSupabaseError(error);
+
+    return {
+      data,
+      error: null,
+      count: 1,
+      status: 200,
+      statusText: "Location Created",
     };
   }
 }
