9b2e6ddf19167e6c311e0bda3145b7e267b85c65
Athina <athina.kantis@gmail.com>
2025-07-22 09:08:12 +0300

    update pagination structure import

--- frontend/src/components/Items/ItemsList.tsx
+++ frontend/src/components/Items/ItemsList.tsx
@@ -4,7 +4,7 @@ import {
   selectAllItems,
   selectItemsLoading,
   selectItemsError,
-  selectItemsLimit,
+  selectItemsPagination,
   fetchOrderedItems,
 } from "../../store/slices/itemsSlice";
 import ItemCard from "./ItemCard";
@@ -21,7 +21,7 @@ import { useDebouncedValue } from "@/hooks/useDebouncedValue";
 const ItemsList: React.FC = () => {
   const filters = useOutletContext<FiltersState>(); // Get filters from context
   const dispatch = useAppDispatch();
-  const pageLimit = useAppSelector(selectItemsLimit);
+  const { limit: pageLimit } = useAppSelector(selectItemsPagination);
   // Redux state selectors
   const items = useAppSelector(selectAllItems);
   const loading = useAppSelector(selectItemsLoading);
