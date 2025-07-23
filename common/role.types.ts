7462620549a70d251f8a30df79d7b9db961ec9ab
Jonathan Gian <jongian90@gmail.com>
2025-07-23 12:20:17 +0300

    Updated role types to a shorter version

--- common/role.types.ts
+++ common/role.types.ts
@@ -1,4 +1,5 @@
 import { Database } from "./database.types";
+import { Enums, Tables } from "./supabase.types";
 
 /**
  * @description Proper Supabase types for role-related entities.
@@ -6,14 +7,14 @@ import { Database } from "./database.types";
  */
 
 // Core role definition
-export type Role = Database["public"]["Tables"]["roles"]["Row"];
+export type Role = Tables<"roles">;
 
 // User-organization-role relationship
-export type UserOrganizationRole =
-  Database["public"]["Tables"]["user_organization_roles"]["Row"];
+export type UserOrganizationRole = Tables<"user_organization_roles">;
 
 // Organization definition
-export type Organization = Database["public"]["Tables"]["organizations"]["Row"];
+export type Organization = Tables<"organizations">;
 
-export type ViewUserRolesWithDetails =
-  Database["public"]["Views"]["view_user_roles_with_details"]["Row"];
+export type ViewUserRolesWithDetails = Tables<"view_user_roles_with_details">;
+
+export type Org_Roles = Enums<"roles_type">
