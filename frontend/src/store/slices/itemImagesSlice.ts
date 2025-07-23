37aa484853e1ecafcd5e980c956e85b3e345e6e0
Athina <athina.kantis@gmail.com>
2025-07-23 17:05:54 +0300

    remove ApiError type, since we have extractErrorMessage

--- frontend/src/store/slices/itemImagesSlice.ts
+++ frontend/src/store/slices/itemImagesSlice.ts
@@ -5,15 +5,6 @@ import { RootState } from "../store";
 import { ErrorContext } from "@/types/common";
 import { extractErrorMessage } from "../utils/errorHandlers";
 
-// Helper type to safely unwrap an Axios‑style error object
-type ApiError = {
-  response?: {
-    data?: {
-      message?: string;
-    };
-  };
-};
-
 interface ItemImagesState {
   images: ItemImage[];
   itemsWithLoadedImages: string[]; // Track which items have loaded images
