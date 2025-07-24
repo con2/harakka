50b3c2ef20ca02ad4a20c67d0c78b6b59b51f4f6
Jonathan Gian <jongian90@gmail.com>
2025-07-24 19:47:59 +0300

    Added supabase back to linting

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   {
     // Add ecosystem.config.js to ignores
-    ignores: ["eslint.config.mjs", "ecosystem.config.js", "dist", "node_modules", "src/types/supabase.types.ts"],
+    ignores: ["eslint.config.mjs", "ecosystem.config.js", "dist", "node_modules"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
