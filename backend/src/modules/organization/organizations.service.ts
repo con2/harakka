8322cac300650ef1f204d10c54db0835e75a2a48
Athina <athina.kantis@gmail.com>
2025-07-25 10:55:31 +0300

    fix lint errors

--- backend/src/modules/organization/organizations.service.ts
+++ backend/src/modules/organization/organizations.service.ts
@@ -9,11 +9,7 @@ import {
   OrganizationInsert,
   OrganizationUpdate,
 } from "./interfaces/organization.interface";
-import {
-  PostgrestError,
-  PostgrestSingleResponse,
-  SupabaseClient,
-} from "@supabase/supabase-js";
+import { PostgrestSingleResponse, SupabaseClient } from "@supabase/supabase-js";
 import { AuthRequest } from "src/middleware/interfaces/auth-request.interface";
 import { getPaginationMeta, getPaginationRange } from "src/utils/pagination";
 import { ApiResponse } from "../../../../common/response.types";
