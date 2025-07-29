fc6f294e3f2dd9821116ac0c87154a4075c7b68f
Athina <athina.kantis@gmail.com>
2025-07-29 14:29:22 +0300

    import + export TagService

--- backend/src/modules/storage-items/storage-items.module.ts
+++ backend/src/modules/storage-items/storage-items.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { StorageItemsController } from "./storage-items.controller";
 import { StorageItemsService } from "./storage-items.service";
 import { SupabaseModule } from "../supabase/supabase.module";
+import { TagModule } from "../tag/tag.module";
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, TagModule],
   controllers: [StorageItemsController],
   providers: [StorageItemsService],
 })
