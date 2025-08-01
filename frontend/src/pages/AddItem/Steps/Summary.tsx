2095ea32942d205ec08e3515bbb1820fc5e739c8
Athina <athina.kantis@gmail.com>
2025-08-01 14:13:04 +0300

    add buttons

--- frontend/src/pages/AddItem/Steps/Summary.tsx
+++ frontend/src/pages/AddItem/Steps/Summary.tsx
@@ -17,6 +17,11 @@ function Summary() {
   const { items } = useAppSelector(selectItemCreation);
   const { lang } = useLanguage();
   const dispatch = useAppDispatch();
+  
+
+  const createNext = () => {
+    
+  }
 
   return (
     <>
@@ -25,43 +30,49 @@ function Summary() {
           New Items Summary
         </p>
         {items.length > 0 ? (
-          <OriginalTable>
-            <OriginalTableHeader>
-              <OriginalTableRow>
-                <OriginalTableHead>Item</OriginalTableHead>
-                <OriginalTableHead>Quantity</OriginalTableHead>
-                <OriginalTableHead>Storage</OriginalTableHead>
-                <OriginalTableHead className="text-right">
-                  Actions
-                </OriginalTableHead>
-              </OriginalTableRow>
-            </OriginalTableHeader>
-            <OriginalTableBody>
-              {items.map((item) => (
-                <OriginalTableRow key={item.id}>
-                  <OriginalTableCell className="font-medium">
-                    {item.translations[lang].item_name}
-                  </OriginalTableCell>
-                  <OriginalTableCell>
-                    {item.items_number_total}
-                  </OriginalTableCell>
-                  <OriginalTableCell>
-                    {item.location_details.name}
-                  </OriginalTableCell>
-                  <OriginalTableCell className="text-right">
-                    Actions here
+          <>
+            <OriginalTable>
+              <OriginalTableHeader>
+                <OriginalTableRow>
+                  <OriginalTableHead>Item</OriginalTableHead>
+                  <OriginalTableHead>Quantity</OriginalTableHead>
+                  <OriginalTableHead>Storage</OriginalTableHead>
+                  <OriginalTableHead className="text-right">
+                    Actions
+                  </OriginalTableHead>
+                </OriginalTableRow>
+              </OriginalTableHeader>
+              <OriginalTableBody>
+                {items.map((item) => (
+                  <OriginalTableRow key={item.id}>
+                    <OriginalTableCell className="font-medium">
+                      {item.translations[lang].item_name}
+                    </OriginalTableCell>
+                    <OriginalTableCell>
+                      {item.items_number_total}
+                    </OriginalTableCell>
+                    <OriginalTableCell>
+                      {item.location_details.name}
+                    </OriginalTableCell>
+                    <OriginalTableCell className="text-right">
+                      Actions here
+                    </OriginalTableCell>
+                  </OriginalTableRow>
+                ))}
+              </OriginalTableBody>
+              <OriginalTableFooter>
+                <OriginalTableRow>
+                  <OriginalTableCell colSpan={4}>
+                    Total items {items.length}
                   </OriginalTableCell>
                 </OriginalTableRow>
-              ))}
-            </OriginalTableBody>
-            <OriginalTableFooter>
-              <OriginalTableRow>
-                <OriginalTableCell colSpan={4}>
-                  Total items {items.length}
-                </OriginalTableCell>
-              </OriginalTableRow>
-            </OriginalTableFooter>
-          </OriginalTable>
+              </OriginalTableFooter>
+            </OriginalTable>
+            <div className="flex justify-end gap-4">
+              <Button variant="default" onClick={createNext}>Create another item</Button>
+              <Button variant="outline">Add items to organization</Button>
+            </div>
+          </>
         ) : (
           <div className="text-center my-6">
             <p className="font-semibold mb-2">No items added yet</p>
