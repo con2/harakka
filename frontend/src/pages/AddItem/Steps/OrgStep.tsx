2ef6ad20458dac14862ad86ce6a630a192b0e8b9
Athina <athina.kantis@gmail.com>
2025-08-04 18:00:58 +0300

    Add org locations loading state + skeleton buttons

--- frontend/src/pages/AddItem/Steps/OrgStep.tsx
+++ frontend/src/pages/AddItem/Steps/OrgStep.tsx
@@ -14,6 +14,7 @@ import { selectCurrentUserOrganizations } from "@/store/slices/rolesSlice";
 import {
   fetchLocationsByOrgId,
   selectCurrentOrgLocations,
+  selectOrgLocationsLoading,
 } from "@/store/slices/organizationLocationsSlice";
 import { useEffect } from "react";
 import {
@@ -22,10 +23,12 @@ import {
   selectOrgLocation,
 } from "@/store/slices/itemsSlice";
 import { setNextStep } from "@/store/slices/uiSlice";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function OrgStep() {
   const { lang } = useLanguage();
   const orgs = useAppSelector(selectCurrentUserOrganizations);
+  const locationLoading = useAppSelector(selectOrgLocationsLoading);
   const orgLocations = useAppSelector(selectCurrentOrgLocations);
   const {
     org: selectedOrg,
@@ -53,7 +56,7 @@ function OrgStep() {
       const newOrg = orgLocations[0];
       dispatch(
         selectOrgLocation({
-          id: newOrg.id,
+          id: newOrg.storage_location_id,
           name: newOrg.storage_locations.name,
           address: newOrg.storage_locations.address,
         }),
@@ -66,6 +69,7 @@ function OrgStep() {
       void dispatch(fetchLocationsByOrgId(selectedOrg?.id));
   }, [dispatch, orgLocations, selectedOrg]);
 
+  /*---------------------render--------------------------------------------------*/
   return (
     <div className="bg-white flex flex-wrap rounded border mt-4 max-w-[900px] p-10 gap-4">
       <div className="flex flex-col gap-2 flex-3">
@@ -73,6 +77,7 @@ function OrgStep() {
           Organization
         </p>
         <div>
+          {/* Select Organization */}
           <Select
             value={selectedOrg?.name ?? ""}
             onValueChange={handleOrgChange}
@@ -103,39 +108,54 @@ function OrgStep() {
 
         {/* Location Selection */}
         <div className="flex justify-between items-center">
-          {orgLocations && (
-            <div className="flex gap-2">
-              {orgLocations.map((loc) => (
-                <Button
-                  key={loc.storage_location_id}
-                  variant={selectedLoc?.id === loc.id ? "outline" : "default"}
-                  className="gap-2"
-                  disabled={items.length > 0}
-                  onClick={() =>
-                    dispatch(
-                      selectOrgLocation({
-                        id: loc.id,
-                        name: loc.storage_locations.name,
-                        address: loc.storage_locations.address,
-                      }),
-                    )
-                  }
-                >
-                  <MapPin />
-                  {loc.storage_locations?.name || `Location #${loc.id}`}
-                </Button>
-              ))}
-              {orgLocations.length > 1 && (
-                <Button
-                  disabled={items.length > 0}
-                  variant={selectedLoc === null ? "outline" : "default"}
-                  onClick={() => dispatch(selectOrgLocation(null))}
-                >
-                  {t.addItem.buttons.chooseLocation[lang]}
-                </Button>
-              )}
-            </div>
-          )}
+          <div className="flex flex-wrap gap-2">
+            {/* Locations of selected org */}
+            {locationLoading
+              ? Array(5)
+                  .fill("")
+                  .map((_, idx) => (
+                    <Skeleton
+                      key={idx}
+                      className={`animate-pulse h-8 rounded-[1rem] bg-muted ${idx % 2 === 0 ? "w-16" : "w-20"} mb-2`}
+                    />
+                  ))
+              : orgLocations.map((loc) => (
+                  <Button
+                    key={loc.storage_location_id}
+                    variant={
+                      selectedLoc?.id === loc.storage_location_id
+                        ? "outline"
+                        : "default"
+                    }
+                    className="gap-2"
+                    disabled={items.length > 0}
+                    onClick={() =>
+                      dispatch(
+                        selectOrgLocation({
+                          id: loc.storage_location_id,
+                          name: loc.storage_locations.name,
+                          address: loc.storage_locations.address,
+                        }),
+                      )
+                    }
+                  >
+                    <MapPin />
+                    {loc.storage_locations?.name ||
+                      `Location #${loc.storage_location_id}`}
+                  </Button>
+                ))}
+
+            {/* Choose location for each item */}
+            {!locationLoading && orgLocations.length > 1 && (
+              <Button
+                disabled={items.length > 0}
+                variant={selectedLoc === null ? "outline" : "default"}
+                onClick={() => dispatch(selectOrgLocation(null))}
+              >
+                {t.addItem.buttons.chooseLocation[lang]}
+              </Button>
+            )}
+          </div>
         </div>
       </div>
       {/* Info and Next Button */}
