b1b4c6aec10083e4af5fd5ad8c3de9d2ee51f343
Athina <athina.kantis@gmail.com>
2025-08-06 10:19:16 +0300

    remove console log

--- frontend/src/components/Items/ItemCard.tsx
+++ frontend/src/components/Items/ItemCard.tsx
@@ -327,11 +327,7 @@ const ItemCard: React.FC<ItemsCardProps> = ({ item }) => {
               variant="outline"
               size="sm"
               onClick={() => {
-                console.log(
-                  `Quantity: ${quantity}, availableQQuantity: ${availabilityInfo.availableQuantity}`,
-                );
                 setQuantity(
-                  // HIER!!
                   Math.min(availabilityInfo.availableQuantity, quantity + 1),
                 );
               }}
