e4757b222645ba273edc9b62ce93269b513d9872
Jonathan Gian <jongian90@gmail.com>
2025-08-07 20:37:53 +0300

    Reverted module

--- backend/src/modules/role/role.module.ts
+++ backend/src/modules/role/role.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common";
 import { RoleController } from "./role.controller";
 import { RoleService } from "./role.service";
 import { JwtModule } from "../jwt/jwt.module";
-import { SupabaseModule } from "../supabase/supabase.module";
 
 @Module({
-  imports: [JwtModule, SupabaseModule],
+  imports: [JwtModule],
   controllers: [RoleController],
   providers: [RoleService],
   exports: [RoleService],
