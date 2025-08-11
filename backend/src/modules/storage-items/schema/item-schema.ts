54093863b888aa27655d9ea644f3360505ab5486
Athina <athina.kantis@gmail.com>
2025-08-11 10:07:03 +0300

    update item schema

--- backend/src/modules/storage-items/schema/item-schema.ts
+++ backend/src/modules/storage-items/schema/item-schema.ts
@@ -5,6 +5,7 @@ export const ItemSchema = z.object({
   en_item_type: z.string().min(1).max(50),
   en_item_description: z.string(),
   fi_item_name: z.string().min(1).max(100),
+  fi_item_type: z.string().min(1).max(50),
   fi_item_description: z.string().min(1).max(50),
   items_number_total: z.coerce
     .number()
