87770b938d39018341cd8a7722e93a8ecc42f379
Jonathan Gian <jongian90@gmail.com>
2025-08-25 13:01:05 +0300

    Fetched migrations and synced up supabase

--- supabase/migrations/20250825082828_remote_schema.sql
+++ supabase/migrations/20250825082828_remote_schema.sql
@@ -1,5 +1,4 @@
 set check_function_bodies = off;
-
 CREATE OR REPLACE FUNCTION auth.custom_access_token2(evt jsonb)
  RETURNS jsonb
  LANGUAGE plpgsql
@@ -40,9 +39,7 @@ begin
            )
          );
 end;
-$function$
-;
-
+$function$;
 CREATE OR REPLACE FUNCTION auth.new_custom_access_token(evt jsonb)
  RETURNS jsonb
  LANGUAGE plpgsql
@@ -112,9 +109,7 @@ begin
   /* Return the merged claim set --------------------------------------------- */
   return evt || jsonb_build_object('app_metadata', meta);
 end;
-$function$
-;
-
+$function$;
 CREATE OR REPLACE FUNCTION auth.new_custom_access_token_v2(evt jsonb)
  RETURNS jsonb
  LANGUAGE plpgsql
@@ -176,9 +171,7 @@ begin
   new_claims := full_evt -> 'app_metadata';
   return jsonb_build_object('claims', new_claims);
 end;
-$function$
-;
-
+$function$;
 CREATE OR REPLACE FUNCTION auth.new_custom_access_token_v3(evt jsonb)
  RETURNS jsonb
  LANGUAGE plpgsql
@@ -225,9 +218,7 @@ begin
   -- Return updated claims payload
   return jsonb_build_object('claims', meta);
 end;
-$function$
-;
-
+$function$;
 CREATE OR REPLACE FUNCTION auth.new_custom_access_token_v4(evt jsonb)
  RETURNS jsonb
  LANGUAGE plpgsql
@@ -285,9 +276,7 @@ begin
   -- Return full claims object
   return jsonb_build_object('claims', claims);
 end;
-$function$
-;
-
+$function$;
 CREATE OR REPLACE FUNCTION auth.test_hook(evt jsonb)
  RETURNS jsonb
  LANGUAGE plpgsql
@@ -298,238 +287,148 @@ begin
   raise notice 'HOOK CALLED: %', evt;
   return evt;
 end;
-$function$
-;
-
+$function$;
 CREATE TRIGGER user_after_insert AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION trg_user_after_insert();
-
 revoke select on table "storage"."iceberg_namespaces" from "anon";
-
 revoke select on table "storage"."iceberg_namespaces" from "authenticated";
-
 revoke delete on table "storage"."iceberg_namespaces" from "service_role";
-
 revoke insert on table "storage"."iceberg_namespaces" from "service_role";
-
 revoke references on table "storage"."iceberg_namespaces" from "service_role";
-
 revoke select on table "storage"."iceberg_namespaces" from "service_role";
-
 revoke trigger on table "storage"."iceberg_namespaces" from "service_role";
-
 revoke truncate on table "storage"."iceberg_namespaces" from "service_role";
-
 revoke update on table "storage"."iceberg_namespaces" from "service_role";
-
 revoke select on table "storage"."iceberg_tables" from "anon";
-
 revoke select on table "storage"."iceberg_tables" from "authenticated";
-
 revoke delete on table "storage"."iceberg_tables" from "service_role";
-
 revoke insert on table "storage"."iceberg_tables" from "service_role";
-
 revoke references on table "storage"."iceberg_tables" from "service_role";
-
 revoke select on table "storage"."iceberg_tables" from "service_role";
-
 revoke trigger on table "storage"."iceberg_tables" from "service_role";
-
 revoke truncate on table "storage"."iceberg_tables" from "service_role";
-
 revoke update on table "storage"."iceberg_tables" from "service_role";
-
 alter table "storage"."iceberg_namespaces" drop constraint "iceberg_namespaces_bucket_id_fkey";
-
 alter table "storage"."iceberg_tables" drop constraint "iceberg_tables_bucket_id_fkey";
-
 alter table "storage"."iceberg_tables" drop constraint "iceberg_tables_namespace_id_fkey";
-
 alter table "storage"."iceberg_namespaces" drop constraint "iceberg_namespaces_pkey";
-
 alter table "storage"."iceberg_tables" drop constraint "iceberg_tables_pkey";
-
 drop index if exists "storage"."iceberg_namespaces_pkey";
-
 drop index if exists "storage"."iceberg_tables_pkey";
-
 drop index if exists "storage"."idx_iceberg_namespaces_bucket_id";
-
 drop index if exists "storage"."idx_iceberg_tables_namespace_id";
-
 drop table "storage"."iceberg_namespaces";
-
 drop table "storage"."iceberg_tables";
-
-
-  create policy "Admin Delete Item Images 6eeiel_0"
+create policy "Admin Delete Item Images 6eeiel_0"
   on "storage"."objects"
   as permissive
   for delete
   to public
 using ((bucket_id = 'item-images-drafts'::text));
-
-
-
-  create policy "Admin Delete Item Images"
+create policy "Admin Delete Item Images"
   on "storage"."objects"
   as permissive
   for delete
   to public
 using (((bucket_id = 'item-images'::text) AND (auth.role() = 'authenticated'::text)));
-
-
-
-  create policy "Admin Insert Item Images"
+create policy "Admin Insert Item Images"
   on "storage"."objects"
   as permissive
   for insert
   to public
 with check (((bucket_id = 'item-images'::text) AND (auth.role() = 'authenticated'::text)));
-
-
-
-  create policy "Admin can delete 6eeiel_0 6eeiel_0"
+create policy "Admin can delete 6eeiel_0 6eeiel_0"
   on "storage"."objects"
   as permissive
   for delete
   to public
 using (((bucket_id = 'item-images'::text) AND (auth.role() = 'authenticated'::text)));
-
-
-
-  create policy "Admin can delete 6eeiel_0"
+create policy "Admin can delete 6eeiel_0"
   on "storage"."objects"
   as permissive
   for delete
   to public
 using (((bucket_id = 'item-images'::text) AND (auth.role() = 'authenticated'::text)));
-
-
-
-  create policy "Give users access to own folder 1skn4k9_0"
+create policy "Give users access to own folder 1skn4k9_0"
   on "storage"."objects"
   as permissive
   for select
   to public
 using (((bucket_id = 'profile-pictures'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
-
-
-
-  create policy "Give users access to own folder 1skn4k9_1"
+create policy "Give users access to own folder 1skn4k9_1"
   on "storage"."objects"
   as permissive
   for insert
   to public
 with check (((bucket_id = 'profile-pictures'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
-
-
-
-  create policy "Give users access to own folder 1skn4k9_2"
+create policy "Give users access to own folder 1skn4k9_2"
   on "storage"."objects"
   as permissive
   for update
   to public
 using (((bucket_id = 'profile-pictures'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
-
-
-
-  create policy "Give users access to own folder 1skn4k9_3"
+create policy "Give users access to own folder 1skn4k9_3"
   on "storage"."objects"
   as permissive
   for delete
   to public
 using (((bucket_id = 'profile-pictures'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
-
-
-
-  create policy "Public Access to Item Images 6eeiel_0"
+create policy "Public Access to Item Images 6eeiel_0"
   on "storage"."objects"
   as permissive
   for select
   to public
 using (true);
-
-
-
-  create policy "Public Access to Item Images"
+create policy "Public Access to Item Images"
   on "storage"."objects"
   as permissive
   for select
   to public
 using ((bucket_id = 'item-images'::text));
-
-
-
-  create policy "Public Read Access"
+create policy "Public Read Access"
   on "storage"."objects"
   as permissive
   for select
   to public
 using ((bucket_id = 'item-images'::text));
-
-
-
-  create policy "TEMP: Allow access 6eeiel_0"
+create policy "TEMP: Allow access 6eeiel_0"
   on "storage"."objects"
   as permissive
   for insert
   to public
 with check ((bucket_id = 'item-images-drafts'::text));
-
-
-
-  create policy "TEMP: Allow access 6eeiel_1"
+create policy "TEMP: Allow access 6eeiel_1"
   on "storage"."objects"
   as permissive
   for update
   to public
 using ((bucket_id = 'item-images-drafts'::text));
-
-
-
-  create policy "TEMP: Allow access 6eeiel_2"
+create policy "TEMP: Allow access 6eeiel_2"
   on "storage"."objects"
   as permissive
   for select
   to public
 using ((bucket_id = 'item-images-drafts'::text));
-
-
-
-  create policy "anyone can do anything 1jxj86u_0"
+create policy "anyone can do anything 1jxj86u_0"
   on "storage"."objects"
   as permissive
   for select
   to public
 using ((bucket_id = 'organization-logo-picture'::text));
-
-
-
-  create policy "anyone can do anything 1jxj86u_1"
+create policy "anyone can do anything 1jxj86u_1"
   on "storage"."objects"
   as permissive
   for insert
   to public
 with check ((bucket_id = 'organization-logo-picture'::text));
-
-
-
-  create policy "anyone can do anything 1jxj86u_2"
+create policy "anyone can do anything 1jxj86u_2"
   on "storage"."objects"
   as permissive
   for update
   to public
 using ((bucket_id = 'organization-logo-picture'::text));
-
-
-
-  create policy "anyone can do anything 1jxj86u_3"
+create policy "anyone can do anything 1jxj86u_3"
   on "storage"."objects"
   as permissive
   for delete
   to public
 using ((bucket_id = 'organization-logo-picture'::text));
-
-
-
