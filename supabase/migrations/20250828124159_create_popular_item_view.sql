0a1bb5371dc8ecbbddca3c5fa6b1659e01936965
Athina <athina.kantis@gmail.com>
2025-08-29 16:21:54 +0300

    update views: replace old with "view_tag_popularity" - add rank percentiles and rank "status".

--- supabase/migrations/20250828124159_create_popular_item_view.sql
+++ supabase/migrations/20250828124159_create_popular_item_view.sql
@@ -5,34 +5,34 @@ JOIN storage_items si ON bi.item_id = si.id
 GROUP BY bi.item_id, si.translations
 ORDER BY times_booked DESC;
 
-CREATE OR REPLACE VIEW public.view_tag_item_count AS 
+CREATE OR REPLACE VIEW public.view_tag_popularity AS
+WITH ranked_tags AS (
+    SELECT
+        t.id AS tag_id,
+        t.translations->'en'->>'name' AS tag_name,
+        t.translations,
+        t.created_at,
+        COUNT(DISTINCT sit.item_id) AS assigned_to,
+        COUNT(bi.booking_id) AS total_bookings,
+        -- Calculate the percentage rank of each tag's total bookings
+        PERCENT_RANK() OVER (ORDER BY COUNT(bi.booking_id) DESC) AS rank_percentile
+    FROM
+        tags t
+    LEFT JOIN
+        storage_item_tags sit ON t.id = sit.tag_id
+    LEFT JOIN
+        booking_items bi ON sit.item_id = bi.item_id
+    GROUP BY
+        t.id, t.translations, t.created_at
+)
 SELECT
-    sit.tag_id,
-    tags.translations->'en'->>'name' as tag_name,
-    COUNT(sit.item_id) as assigned_to
+    *,
+    CASE
+        WHEN rank_percentile <= 0.05 THEN 'very popular'
+        WHEN rank_percentile <= 0.25 THEN 'popular'
+        ELSE ''
+    END AS popularity_rank
 FROM
-    storage_item_tags sit
-JOIN
-    tags on tags.id = sit.tag_id
-GROUP BY
-    sit.tag_id, tags.translations->'en'->>'name'
-ORDER BY
-    assigned_to DESC;
-
-CREATE OR REPLACE VIEW public.view_most_popular_tags AS
-SELECT
-    t.id AS tag_id,
-    t.translations->'en'->>'name' AS tag_name,
-    COUNT(bi.booking_id) AS total_bookings,
-    t.translations,
-    t.created_at
-FROM
-    tags t
-JOIN
-    storage_item_tags sit ON t.id = sit.tag_id
-JOIN
-    booking_items bi ON sit.item_id = bi.item_id
-GROUP BY
-    t.id, t.translations->'en'->>'name'
+    ranked_tags
 ORDER BY
     total_bookings DESC;
