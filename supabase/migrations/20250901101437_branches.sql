31db14b854c20b6cf6385cb10908cf57062be837
Jonathan Gian <jongian90@gmail.com>
2025-09-01 15:53:13 +0300

    Removed branches

--- supabase/migrations/20250901101437_branches.sql
+++
@@ -1,6 +0,0 @@
-create table branches (
-    id uuid primary key default uuid_generate_v4(),
-    name text not null,
-    location text not null,
-    created_at timestamp with time zone default current_timestamp
-);
