9e4e07971b61007fb4e9b2946ef132d9247144e2
Vladimir Beliakov <ermegilius@gmail.com>
2025-09-12 11:10:17 +0300

    add one more new column to test_pr_update

--- supabase/migrations/20250911120101_create_test_pr_update_table.sql
+++ supabase/migrations/20250911120101_create_test_pr_update_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE public.test_pr_update (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     brand_new_column VARCHAR(100),
+    this_one_even_newer INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
