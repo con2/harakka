a17ec28c9d1ab12df373e849d86c5df622eef16a
Jonathan Gian <jongian90@gmail.com>
2025-09-17 17:29:29 +0300

    Dropped ANON org RSL

--- supabase/migrations/20250917133624_Org_RLS_Drop_Anon.sql
+++ supabase/migrations/20250917133624_Org_RLS_Drop_Anon.sql
@@ -1,1 +1,7 @@
 DROP POLICY IF EXISTS "Anonymous users cannot access user organization roles" ON public.user_organization_roles;
+
+CREATE POLICY "Annonymous users can access user organization roles"
+  ON public.user_organization_roles
+  FOR ALL 
+  TO anon
+  USING (true);
