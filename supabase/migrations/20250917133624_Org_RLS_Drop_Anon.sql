09a9ee0447ad1ca803d81a18663b38b4f635ba76
Jonathan Gian <jongian90@gmail.com>
2025-09-17 17:32:16 +0300

    Removed migration

--- supabase/migrations/20250917133624_Org_RLS_Drop_Anon.sql
+++
@@ -1,7 +0,0 @@
-DROP POLICY IF EXISTS "Anonymous users cannot access user organization roles" ON public.user_organization_roles;
-
-CREATE POLICY "Annonymous users can access user organization roles"
-  ON public.user_organization_roles
-  FOR ALL 
-  TO anon
-  USING (true);
