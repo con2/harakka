9881969395015022e7b8ca94ffd8016d4a599281
Maria Aluko <maria.alukoo@gmail.com>
2025-09-18 19:19:20 +0300

    translations

--- frontend/src/translations/modules/myBookingsPage.ts
+++ frontend/src/translations/modules/myBookingsPage.ts
@@ -10,5 +10,23 @@ export const myBookingsPage = {
       en: common.back.en,
       fi: common.back.fi,
     },
+    edit: {
+      en: "Edit Booking",
+      fi: "Muokkaa varausta",
+    },
+  },
+  headings: {
+    createdAt: {
+      en: "Created at",
+      fi: "Luotu",
+    },
+    bookingDates: {
+      en: "Booking Dates",
+      fi: "Varauspäivät",
+    },
+    availability: {
+      en: "Total available to book",
+      fi: "Varattavissa yhteensä",
+    },
   },
 };
