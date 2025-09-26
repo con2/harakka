2e996d56407404adc253976dbf9fabf9f98da951
Athina <athina.kantis@gmail.com>
2025-09-26 14:14:17 +0300

    add different titles and descriptions for the errors

--- frontend/src/translations/modules/error.ts
+++ frontend/src/translations/modules/error.ts
@@ -1,7 +1,27 @@
 export const error = {
-  title: {
-    en: "Something went wrong",
-    fi: "Jotain meni pieleen",
+  titles: {
+    "not-found": {
+      en: "Page not found",
+      fi: "",
+    },
+    "server-error": {
+      en: "Internal server error",
+      fi: "",
+    },
+    general: {
+      en: "Something went wrong",
+      fi: "Jotain meni pieleen",
+    },
+  },
+  descriptions: {
+    "not-found": {
+      en: "The magpies couldn't find your page... Did you enter the correct one?",
+      fi: "Harakat eivät löytäneet sivuasi... Annoitko oikean osoitteen?",
+    },
+    "server-error": {
+      en: "The magpies did not want to cooperate.. Contact us if the error persists",
+      fi: "Harakat eivät suostuneet yhteistyöhön... Ota meihin yhteyttä, jos virhe toistuu.",
+    },
   },
   buttons: {
     back: {
