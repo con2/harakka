7bd065055df4c6a609308f3164ee0a052d82f0f8
Athina <athina.kantis@gmail.com>
2025-10-06 22:11:44 +0300

    add error translations

--- frontend/src/translations/modules/orgStep.ts
+++ frontend/src/translations/modules/orgStep.ts
@@ -55,4 +55,18 @@ export const orgStep = {
       fi: "Organisaatiolle ei löytynyt sijainteja.",
     },
   },
+  messages: {
+    notACSV: {
+      en: "{file_name} is not a CSV file",
+      fi: "{file_name} ei ole CSV-tiedosto",
+    },
+    noFileSelected: {
+      en: "No file selected.",
+      fi: "Ei tiedostoa valittu.",
+    },
+    failedProcess: {
+      en: "Failed to process file. Try again and contact support if the problem persists.",
+      fi: "Tiedoston käsittely epäonnistui. Yritä uudelleen ja ota yhteyttä tukeen, jos ongelma jatkuu.",
+    },
+  },
 };
