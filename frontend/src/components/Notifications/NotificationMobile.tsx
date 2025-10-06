03cf2b7c1d15940a36ec9f2533c974c556e42c2e
stabjana <garschtubald@gmail.com>
2025-10-06 14:08:44 +0300

    change sliding direction of notifications mobile view

--- frontend/src/components/Notifications/NotificationMobile.tsx
+++ frontend/src/components/Notifications/NotificationMobile.tsx
@@ -71,11 +71,7 @@ export const NotificationMobile: React.FC<Props> = ({
       </Button>
       {/* Notification Panel */}
       <Sheet open={panelOpen} onOpenChange={setPanelOpen}>
-        <SheetContent
-          side="right"
-          hideClose
-          className="w-[90vw] sm:max-w-sm p-0"
-        >
+        <SheetContent side="top" hideClose className="w-[90vw] sm:max-w-sm p-0">
           <div className="p-3 border-b flex items-center justify-between gap-2 flex-wrap">
             {/* Notifications title (SheetTitle for accessibility) */}
             <div className="flex flex-col min-w-0">
